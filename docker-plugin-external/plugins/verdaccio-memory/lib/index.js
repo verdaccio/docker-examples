d22dacbadd3523479374e3c8a3170c7514de6669
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2018-07-18 20:13:43 +0200

    feat: add docker using external plugins

---
+++ docker-plugin-external/plugins/verdaccio-memory/lib/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.LocalMemory = undefined;
+
+var _localMemory = require('./local-memory');
+
+var _localMemory2 = _interopRequireDefault(_localMemory);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.LocalMemory = _localMemory2.default;
+exports.default = _localMemory2.default;
---
+++ docker-plugin-external/plugins/verdaccio-memory/node_modules/@verdaccio/streams/jest.config.js
@@ -0,0 +1,11 @@
+/* eslint comma-dangle: 0 */
+
+module.exports = {
+  'name': 'verdaccio-streams',
+  'verbose': true,
+  'collectCoverage': true,
+  'coveragePathIgnorePatterns': [
+    'node_modules',
+    'fixtures'
+  ]
+};
